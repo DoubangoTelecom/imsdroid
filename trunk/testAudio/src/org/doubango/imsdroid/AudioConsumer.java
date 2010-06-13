e48585110d6b8b8cbabd99031e68799aeacebe8a
bossiel@yahoo.fr <bossiel@yahoo.fr@088c1f0a-aade-f869-c05e-f0c1bd8791d0>
2010-06-13 04:14:07 +0000

    Add Audio Producer and Consumer Proxies into testAudio project.

--- trunk/testAudio/src/org/doubango/imsdroid/AudioConsumer.java
+++ trunk/testAudio/src/org/doubango/imsdroid/AudioConsumer.java
@@ -1,8 +1,28 @@
+/*
+* Copyright (C) 2010 Mamadou Diop.
+*
+* Contact: Mamadou Diop <diopmamadou(at)doubango.org>
+*	
+* This file is part of imsdroid Project (http://code.google.com/p/imsdroid)
+*
+* imsdroid is free software: you can redistribute it and/or modify it under the terms of 
+* the GNU General Public License as published by the Free Software Foundation, either version 3 
+* of the License, or (at your option) any later version.
+*	
+* imsdroid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License along 
+* with this program; if not, write to the Free Software Foundation, Inc., 
+* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
 package org.doubango.imsdroid;
 
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.nio.ByteBuffer;
+
+import org.doubango.tinyWRAP.ProxyAudioConsumer;
 
 import android.media.AudioFormat;
 import android.media.AudioManager;
@@ -10,117 +30,132 @@ import android.media.AudioTrack;
 import android.media.AudioTrack.OnPlaybackPositionUpdateListener;
 import android.util.Log;
 
-// recorder : http://markmail.org/message/zf5iafifjfstjcwa#query:setNotificationMarkerPosition+page:1+mid:gnz4dcuvd5477qt5+state:results
-
-public class AudioConsumer extends Thread{
+public class AudioConsumer{
 	
 	private static String TAG = AudioConsumer.class.getCanonicalName();
+	private static int factor = 5;
+	private static int streamType = AudioManager.STREAM_MUSIC; /* STREAM_VOICE_CALL */
 	
-	public static final int sampleRateInHz = 8000;
-	public static final int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
-	public static final int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
-	public static final int avgBytesPerSec = 0;
-	public static final int ptime = 20;
-	
-	private int bytes_per_notif;
 	private int bufferSize;
-	private AudioTrack audioTrack;
-
+	private int shorts_per_notif;
+	private final MyProxyAudioConsumer proxyAudioConsumer;
 	
-	private boolean running;
+	private AudioTrack track;
+	private ByteBuffer chunck;
+	
+	static {
+		MyProxyAudioConsumer.registerPlugin();
+	}
 	
 	public AudioConsumer(){
-		this.bufferSize = AudioTrack.getMinBufferSize(AudioConsumer.sampleRateInHz, AudioConsumer.channelConfig, AudioConsumer.audioFormat);
-		this.bytes_per_notif = (AudioConsumer.sampleRateInHz * AudioConsumer.ptime)/1000;
-
+		this.proxyAudioConsumer = new MyProxyAudioConsumer(this);
 	}	
 
-//	 public void StartRecord() { recorder.startRecording(); }
-//	 public void StopRecord() { recorder.stop(); }
-//	 public void ReleaseRecord() { recorder.release(); } 
-	 
-	@Override
-	public synchronized void start() {
-		// TODO Auto-generated method stub
-		super.start();
-	}
-
-	public synchronized void stopMe() {
-		this.running = false;
+	public void setActive(){
+		this.proxyAudioConsumer.setActivate();
 	}
 	
+	private synchronized int prepare(int ptime, int rate) {
+		Log.d(AudioConsumer.TAG, "prepare()");
+		
+		int minBufferSize = AudioTrack.getMinBufferSize(rate, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+		this.shorts_per_notif = (rate * ptime)/1000;
+		this.bufferSize = minBufferSize + (this.shorts_per_notif - (minBufferSize % this.shorts_per_notif));
+		this.chunck = ByteBuffer.allocateDirect(this.shorts_per_notif*2);
+		
+		this.track = new AudioTrack(AudioConsumer.streamType,
+				rate, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT,
+				(this.bufferSize * AudioConsumer.factor), AudioTrack.MODE_STREAM);
+		if(this.track.getState() == AudioTrack.STATE_INITIALIZED){
+			this.track.setPositionNotificationPeriod(this.shorts_per_notif);
+			this.track.setPlaybackPositionUpdateListener(this.playbackPositionUpdateListener);
+			return 0;
+		}
+		else{
+			Log.e(AudioConsumer.TAG, "prepare() failed");
+			return -1;
+		}
+	}
+
+	private synchronized int start() {
+		Log.d(AudioConsumer.TAG, "start()");
+		if(this.track != null){
+			this.track.play();
+			/* Mandatory in order to have first notifications 
+			 * FIXME: Why when we write bufferSize bytes (like we do in the recorder) the callback function is not called ?*/
+			this.track.write(new byte[this.bufferSize*AudioConsumer.factor], 0, this.bufferSize*AudioConsumer.factor);
+			return 0;
+		}
+		return -1;
+	}
+	
+	private synchronized int pause() {
+		Log.d(AudioConsumer.TAG, "pause()");
+		if(this.track != null){
+			this.track.pause();
+			return 0;
+		}
+		return -1;
+	}
+
+	private synchronized int stop() {
+		Log.d(AudioConsumer.TAG, "stop()");
+		if(this.track != null){
+			this.track.stop();
+			this.track.release();
+			this.track = null;
+			
+			System.gc();
+			return 0;
+		}
+		return -1;
+	}
 	
 	private OnPlaybackPositionUpdateListener playbackPositionUpdateListener = new OnPlaybackPositionUpdateListener()
 	{
 		@Override
-		public void onMarkerReached(AudioTrack track) {
-			int i = 0;
-			i++;
-			Log.d(AudioConsumer.TAG, "onMarkerReached");
-		}
-
-		@Override
-		public void onPeriodicNotification(AudioTrack track) {
-			Log.d(AudioConsumer.TAG, "onPeriodicNotification");
-		}
-	};
-	
-	Runnable rrr = new Runnable()
-	{
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			byte []bytes = new byte[bufferSize];
-			new Random().nextBytes(bytes);
-			
-			
-			audioTrack.write(bytes, 0, bytes.length);
-		}
-	};
-	
-	@Override
-	public void run() {
-		/* Set Thread Priority */
-		android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
-		
-		this.audioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, /* AudioManager.STREAM_MUSIC */
-				AudioConsumer.sampleRateInHz,
-				AudioConsumer.channelConfig,
-				AudioConsumer.audioFormat,
-                this.bufferSize,
-                AudioTrack.MODE_STREAM);
-		
-		int ret = this.audioTrack.setPositionNotificationPeriod(160/2);
-		ret = this.audioTrack.setNotificationMarkerPosition(160);
-		this.audioTrack.setPlaybackPositionUpdateListener(this.playbackPositionUpdateListener); 
-		
-		byte []bytes = new byte[742];
-		new Random().nextBytes(bytes);
-		
-		Log.d(AudioConsumer.TAG, "before write");
-		audioTrack.write(bytes, 0, bytes.length);
-		Log.d(AudioConsumer.TAG, "after write");
-		
-		//audioTrack.setPlaybackHeadPosition(0);
-
-		//this.audioTrack.flush();
-		this.audioTrack.play();
-		Log.d(AudioConsumer.TAG, "after play");
-		
-		while(true){
-			try {
-				
-				//new Thread(this.rrr).start();
-				sleep(5000);
-				
-				//bytes = new byte[bytes_per_notif];
-				//new Random().nextBytes(bytes);
-				//audioTrack.write(bytes, 0, bytes.length);
-				//audioTrack.play();				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		public void onPeriodicNotification(AudioTrack track) {			
+			long sizeInBytes = AudioConsumer.this.proxyAudioConsumer.pull(AudioConsumer.this.chunck, AudioConsumer.this.chunck.capacity());			
+			final byte[] bytes = new byte[AudioConsumer.this.chunck.capacity()];		
+			if(sizeInBytes >0){ /* Otherwise it's silence */
+				AudioConsumer.this.chunck.get(bytes);
 			}
+			AudioConsumer.this.track.write(bytes, 0, bytes.length);
+			AudioConsumer.this.chunck.rewind();
 		}
-	}	
+
+		@Override
+		public void onMarkerReached(AudioTrack track) {
+		}
+	};
+	
+	
+	class MyProxyAudioConsumer extends ProxyAudioConsumer
+    {
+		private final AudioConsumer consumer;
+		
+		private MyProxyAudioConsumer(AudioConsumer consumer){
+			this.consumer = consumer;
+		}
+		
+		@Override
+		public int pause() {
+			return this.consumer.pause();
+		}
+
+		@Override
+		public int prepare(int ptime, int rate, int channels) {
+			return this.consumer.prepare(ptime, rate);
+		}
+
+		@Override
+		public int start() {
+			return this.consumer.start();
+		}
+
+		@Override
+		public int stop() {
+			return this.consumer.stop();
+		}
+    }
 }
--- trunk/testAudio/src/org/doubango/imsdroid/Main.java
+++ trunk/testAudio/src/org/doubango/imsdroid/Main.java
@@ -1,20 +1,166 @@
+/*
+* Copyright (C) 2010 Mamadou Diop.
+*
+* Contact: Mamadou Diop <diopmamadou(at)doubango.org>
+*	
+* This file is part of imsdroid Project (http://code.google.com/p/imsdroid)
+*
+* imsdroid is free software: you can redistribute it and/or modify it under the terms of 
+* the GNU General Public License as published by the Free Software Foundation, either version 3 
+* of the License, or (at your option) any later version.
+*	
+* imsdroid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU General Public License for more details.
+*	
+* You should have received a copy of the GNU General Public License along 
+* with this program; if not, write to the Free Software Foundation, Inc., 
+* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
 package org.doubango.imsdroid;
 
+import org.doubango.tinyWRAP.CallSession;
+import org.doubango.tinyWRAP.DDebugCallback;
+import org.doubango.tinyWRAP.RegistrationEvent;
+import org.doubango.tinyWRAP.RegistrationSession;
+import org.doubango.tinyWRAP.SipCallback;
+import org.doubango.tinyWRAP.SipStack;
+
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
+    
+	DDebugCallback debugCallBack;
+	MySipCallback sipCallBack;
+	SipStack sipStack;
+	RegistrationSession regSession;
+	CallSession callSession;
+	
+	AudioConsumer audioConsumer;
+	AudioProducer audioProducer;
+	
+	Button btnCall;
+	Button btnRegister;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        @SuppressWarnings("unused")
+		boolean success;
+       
         
-        AudioConsumer consumer = new AudioConsumer();
-        consumer.start();
+        //audioConsumer = new AudioConsumer();
+        //audioConsumer.prepare(20, 8000, AudioManager.STREAM_MUSIC);
+        //audioConsumer.start();
         
-        int i = 0;
-        i++;
+        this.debugCallBack = new DDebugCallback();
+        this.sipCallBack = new MySipCallback();
+        this.sipStack = new SipStack(this.sipCallBack, "sip:ericsson.com", "mamadou", "sip:mamadou@ericsson.com");
+        this.sipStack.setDebugCallback(this.debugCallBack);
+        
+        success = this.sipStack.isValid();
+        success = this.sipStack.setProxyCSCF("192.168.0.13", 5081, "tcp", "ipv4");
+        this.sipStack.setLocalIP(this.getLocalIP(false));
+        
+        success = this.sipStack.start();
+        
+        // Audio Consumer/Producer
+        audioConsumer = new AudioConsumer();
+        audioProducer = new AudioProducer();
+              
+        this.btnRegister = (Button)this.findViewById(R.id.ButtonRegister);
+        this.btnRegister.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(regSession == null){
+					regSession = new RegistrationSession(sipStack);
+				}
+			    regSession.setExpires(30);
+			    regSession.Register();
+			}
+        });
+        
+        this.btnCall = (Button)this.findViewById(R.id.ButtonCall);
+        this.btnCall.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(callSession == null){
+					callSession = new CallSession(sipStack);
+				}
+				audioConsumer.setActive();
+				audioProducer.setActive();
+				callSession.Call("sip:bob@ericsson.com");
+			}
+        });
+        
+        
+        
+        //AudioConsumer consumer = new AudioConsumer();
+        //consumer.prepare(20, 8000, AudioManager.STREAM_MUSIC); /* STREAM_VOICE_CALL */
+        //consumer.start();
     }
+    
+    
+    public String getLocalIP(boolean ipv6){
+		
+		if(ipv6){
+			return null;
+		}
+		
+		WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+		int ipAddress = wifiInfo.getIpAddress();
+		
+		if(ipAddress != 0){
+			return String.format("%d.%d.%d.%d",
+					(ipAddress>>0)&0xFF,
+					(ipAddress>>8)&0xFF,
+					(ipAddress>>16)&0xFF,
+					(ipAddress>>24)&0xFF
+					);
+		}
+		else{
+			return null;
+		}
+	}
+    
+    private class MySipCallback extends SipCallback
+    {
+    	public MySipCallback()
+    	{
+    		super();
+    	}
+		@Override
+		public int OnRegistrationEvent(RegistrationEvent e) {
+			Log.d("Test Audio ()", String.format("OnRegistrationEvent (code=%d) and (phrase=%s)", e.getCode(), e.getPhrase()));
+			
+			return 0;
+		}
+    }
+    
+    static {
+		try {
+			//System.loadLibrary("tinyWRAP");
+			System.load(String.format("/data/data/%s/lib/libtinyWRAP.so", Main.class
+					.getPackage().getName()));
+
+		} catch (UnsatisfiedLinkError e) {
+			Log.e(Main.class.getCanonicalName(),
+					"Native code library failed to load.\n" + e.getMessage());
+		} catch (Exception e) {
+			Log.e(Main.class.getCanonicalName(),
+					"Native code library failed to load.\n" + e.getMessage());
+		}
+	}
 }
