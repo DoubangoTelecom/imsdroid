a393490441439d2304ddd1fdc30f5d44fe7cbaeb
bossiel <bossiel@yahoo.fr>
2014-05-12 02:32:48 +0000

    Align with Doubango 1089 (Support for TURN)

--- branches/2.0/imsdroid/src/org/doubango/imsdroid/Screens/ScreenNatt.java
+++ branches/2.0/imsdroid/src/org/doubango/imsdroid/Screens/ScreenNatt.java
@@ -38,14 +38,17 @@ public class ScreenNatt extends BaseScreen {
 	private final static String TAG = ScreenNatt.class.getCanonicalName();
 	
 	private CheckBox mCbHackAoR;
-	private CheckBox mCbEnableStun;
-	private RelativeLayout mRlEnableStun;
+	private CheckBox mCbEnableStunForSIP;
+	private CheckBox mCbEnableStunForICE;
+	private CheckBox mCbEnableTurnForICE;
 	private CheckBox mCbEnableIce;
 	private RelativeLayout mRlStunServer;
 	private RadioButton mRbDiscoStun;
 	private RadioButton mRbSetStun;
-	private EditText mEtStunServer;
+	private EditText mEtStunStunHostname;
 	private EditText mEtStunPort;
+	private EditText mEtStunUsername;
+	private EditText mEtStunPassword;
 	
 	private final INgnConfigurationService mConfigurationService;
 	
@@ -61,22 +64,29 @@ public class ScreenNatt extends BaseScreen {
         
         // get controls
         mCbHackAoR = (CheckBox)findViewById(R.id.screen_natt_checkBox_hack_aor);
-        mCbEnableStun = (CheckBox)findViewById(R.id.screen_natt_checkBox_stun);
-        mRlEnableStun = (RelativeLayout)findViewById(R.id.screen_natt_relativeLayout_stun);
-        mCbEnableIce = (CheckBox)findViewById(R.id.screen_natt_checkBox_ice);
+        mCbEnableStunForSIP = (CheckBox)findViewById(R.id.screen_natt_checkBox_stun_for_sip);
+        mCbEnableStunForICE = (CheckBox)findViewById(R.id.screen_natt_checkBox_stun_for_ice);
+        mCbEnableTurnForICE = (CheckBox)findViewById(R.id.screen_natt_checkBox_turn_for_ice);
         mRlStunServer = (RelativeLayout)findViewById(R.id.screen_natt_relativeLayout_stun_server);
+        mCbEnableIce = (CheckBox)findViewById(R.id.screen_natt_checkBox_ice);
         mRbDiscoStun = (RadioButton)findViewById(R.id.screen_natt_radioButton_stun_disco);
         mRbSetStun = (RadioButton)findViewById(R.id.screen_natt_radioButton_stun_set);
-        mEtStunServer = (EditText)findViewById(R.id.screen_natt_editText_stun_server);
+        mEtStunStunHostname = (EditText)findViewById(R.id.screen_natt_editText_stun_server);
         mEtStunPort = (EditText)findViewById(R.id.screen_natt_editText_stun_port);
+        mEtStunUsername = (EditText)findViewById(R.id.screen_natt_editText_stun_username);
+        mEtStunPassword = (EditText)findViewById(R.id.screen_natt_editText_stun_password);
         
         // load values from configuration file (do it before adding UI listeners)
         mCbHackAoR.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_HACK_AOR, NgnConfigurationEntry.DEFAULT_NATT_HACK_AOR));
-        mCbEnableStun.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_USE_STUN, NgnConfigurationEntry.DEFAULT_NATT_USE_STUN));
+        mCbEnableStunForSIP.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_USE_STUN_FOR_SIP, NgnConfigurationEntry.DEFAULT_NATT_USE_STUN_FOR_SIP));
+        mCbEnableStunForICE.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_USE_STUN_FOR_ICE, NgnConfigurationEntry.DEFAULT_NATT_USE_STUN_FOR_ICE));
+        mCbEnableTurnForICE.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_USE_TURN_FOR_ICE, NgnConfigurationEntry.DEFAULT_NATT_USE_TURN_FOR_ICE));
         mCbEnableIce.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_USE_ICE, NgnConfigurationEntry.DEFAULT_NATT_USE_ICE));
         mRbDiscoStun.setChecked(mConfigurationService.getBoolean(NgnConfigurationEntry.NATT_STUN_DISCO, NgnConfigurationEntry.DEFAULT_NATT_STUN_DISCO));
-        mEtStunServer.setText(mConfigurationService.getString(NgnConfigurationEntry.NATT_STUN_SERVER, NgnConfigurationEntry.DEFAULT_NATT_STUN_SERVER));
+        mEtStunStunHostname.setText(mConfigurationService.getString(NgnConfigurationEntry.NATT_STUN_SERVER, NgnConfigurationEntry.DEFAULT_NATT_STUN_SERVER));
         mEtStunPort.setText(mConfigurationService.getString(NgnConfigurationEntry.NATT_STUN_PORT, Integer.toString(NgnConfigurationEntry.DEFAULT_NATT_STUN_PORT)));
+        mEtStunUsername.setText(mConfigurationService.getString(NgnConfigurationEntry.NATT_STUN_USERNAME, NgnConfigurationEntry.DEFAULT_NATT_STUN_USERNAME));
+        mEtStunPassword.setText(mConfigurationService.getString(NgnConfigurationEntry.NATT_STUN_PASSWORD, NgnConfigurationEntry.DEFAULT_NATT_STUN_PASSWORD));
         
         // mRlEnableStun.setVisibility(mCbEnableStun.isChecked() ? View.VISIBLE : View.INVISIBLE);
         mRlStunServer.setVisibility(mRbSetStun.isChecked() ? View.VISIBLE : View.INVISIBLE);
@@ -85,13 +95,18 @@ public class ScreenNatt extends BaseScreen {
         addConfigurationListener(mCbHackAoR);
         /* addConfigurationListener(cbEnableStun); */
         addConfigurationListener(mCbEnableIce);
+        addConfigurationListener(mCbEnableStunForSIP);
+        addConfigurationListener(mCbEnableStunForICE);
+        addConfigurationListener(mCbEnableTurnForICE);
         /* addConfigurationListener(rbDiscoStun);
         addConfigurationListener(rbSetStun); */
-        addConfigurationListener(mEtStunServer);
+        addConfigurationListener(mEtStunStunHostname);
         addConfigurationListener(mEtStunPort);
+        addConfigurationListener(mEtStunUsername);
+        addConfigurationListener(mEtStunPassword);
         
         
-        mCbEnableStun.setOnCheckedChangeListener(mCbEnableStun_OnCheckedChangeListener);
+        mCbEnableStunForSIP.setOnCheckedChangeListener(mCbEnableStun_OnCheckedChangeListener);
         mRbSetStun.setOnCheckedChangeListener(mRbSetStun_OnCheckedChangeListener);
 	}
 
@@ -99,11 +114,15 @@ public class ScreenNatt extends BaseScreen {
 		if(super.mComputeConfiguration){
 			
 			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_HACK_AOR, mCbHackAoR.isChecked());
-			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_USE_STUN, mCbEnableStun.isChecked());
+			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_USE_STUN_FOR_SIP, mCbEnableStunForSIP.isChecked());
+			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_USE_STUN_FOR_ICE, mCbEnableStunForICE.isChecked());
+			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_USE_TURN_FOR_ICE, mCbEnableTurnForICE.isChecked());
 			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_USE_ICE, mCbEnableIce.isChecked());
 			mConfigurationService.putBoolean(NgnConfigurationEntry.NATT_STUN_DISCO, mRbDiscoStun.isChecked());
-			mConfigurationService.putString(NgnConfigurationEntry.NATT_STUN_SERVER, mEtStunServer.getText().toString());
+			mConfigurationService.putString(NgnConfigurationEntry.NATT_STUN_SERVER, mEtStunStunHostname.getText().toString());
 			mConfigurationService.putString(NgnConfigurationEntry.NATT_STUN_PORT, mEtStunPort.getText().toString());
+			mConfigurationService.putString(NgnConfigurationEntry.NATT_STUN_USERNAME, mEtStunUsername.getText().toString());
+			mConfigurationService.putString(NgnConfigurationEntry.NATT_STUN_PASSWORD, mEtStunPassword.getText().toString());
 	        
 			// Compute
 			if(!mConfigurationService.commit()){
@@ -111,7 +130,11 @@ public class ScreenNatt extends BaseScreen {
 			}
 			else{
 				MediaSessionMgr.defaultsSetIceEnabled(mCbEnableIce.isChecked());
-				MediaSessionMgr.defaultsSetStunEnabled(mCbEnableStun.isChecked());
+				MediaSessionMgr.defaultsSetIceStunEnabled(mCbEnableStunForICE.isChecked());
+				MediaSessionMgr.defaultsSetIceTurnEnabled(mCbEnableTurnForICE.isChecked());
+				MediaSessionMgr.defaultsSetStunEnabled(mCbEnableStunForSIP.isChecked());
+				MediaSessionMgr.defaultsSetStunServer(mEtStunStunHostname.getText().toString(), Integer.parseInt(mEtStunPort.getText().toString()));
+				MediaSessionMgr.defaultsSetStunCred(mEtStunUsername.getText().toString(), mEtStunPassword.getText().toString());
 			}
 			
 			super.mComputeConfiguration = false;
